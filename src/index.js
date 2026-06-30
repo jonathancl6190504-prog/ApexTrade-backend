cd9d0fa326b74029a5e35d05354c5f6cc15a8f7e
jonathancl6190504-prog <jonathancl6190504@gmail.com>
2026-06-30 02:04:58 -0700

    Serve static files from 'public' directory

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 const wsServer = new WSServer();
 const bot = new TradingBot({
